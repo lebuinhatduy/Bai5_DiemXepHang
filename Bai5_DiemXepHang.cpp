7bbd56600b7975f75378fa7484288d2952e5eb5e
lebuinhatduy <lebuinhatduyyy@gmail.com>
2024-11-04 16:45:54 +0700

    Add project files.

---
+++ Bai5_DiemXepHang.cpp
@@ -0,0 +1,35 @@
+// Bai5_DiemXepHang.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "CHUONG TRIH XEP HANG TU DIEM\n";
+    double diem;
+    cout << "Nhap diem: "; cin >> diem;
+
+    if (diem < 5) {
+        cout << "Yeu";
+    }else if (diem <= 6) {
+        cout << "TB";
+    }else if (diem < 8) {
+        cout << "Kha";
+    }else {
+        cout << "Gioi";
+    }
+
+    return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
